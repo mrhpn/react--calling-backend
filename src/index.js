897a22c324e4c59de240451f77d9f99b5a684974
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2021-10-01 23:15:47 +0630

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import logger from './services/loggingService';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+logger.init();
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
---
+++ src/services/loggingService.js
@@ -0,0 +1,23 @@
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
+
+function init() {
+  Sentry.init({
+    dsn: 'https://8ec7e5a0c5954c078c9d5f6039f55529@o1022347.ingest.sentry.io/5988515',
+    integrations: [new Integrations.BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
+
+function log(error) {
+  Sentry.captureException(error);
+}
+
+export default {
+  init,
+  log,
+};
